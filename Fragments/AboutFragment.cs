8da495c33178389d78e074457a26416209deee70
Mohammad Amin Mollazadeh <the.madamin20@gmail.com>
2020-12-26 14:07:55 +0330

    Add Instagram API version to about page

--- Fragments/AboutFragment.cs
+++ Fragments/AboutFragment.cs
@@ -1,11 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
-
 using Android.Content;
-
 using Google.Android.Material.Button;
 using Google.Android.Material.TextView;
-
 using Madamin.Unfollow.Instagram;
 
 namespace Madamin.Unfollow.Fragments
@@ -22,20 +20,46 @@ namespace Madamin.Unfollow.Fragments
         {
             Title = GetString(Resource.String.title_about);
 
-            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            Debug.Assert(
+                Activity?.PackageManager != null &&
+                Activity.PackageName != null
+            );
 
-            var tvVersion = e.View.FindViewById<MaterialTextView>(Resource.Id.fragment_about_version);
+            var package = Activity.PackageManager.GetPackageInfo(
+                Activity.PackageName,
+                0);
+
+            Debug.Assert(package != null);
+
+            var igVersion = typeof(InstagramApiSharp.API.IInstaApi)
+                .Assembly.GetName();
+
+            var tvVersion = e.View.FindViewById<MaterialTextView>(Resource.Id.fragment_about_app_version);
+            var tvIgShVersion = e.View.FindViewById<MaterialTextView>(Resource.Id.fragment_about_instasharp_version);
+
             var btnTelegram = e.View.FindViewById<MaterialButton>(Resource.Id.fragment_about_telegram);
             var btnInstagram = e.View.FindViewById<MaterialButton>(Resource.Id.fragment_about_instagram);
             var btnGithub = e.View.FindViewById<MaterialButton>(Resource.Id.fragment_about_github);
 
-            if (tvVersion == null ||
-                btnTelegram == null ||
-                btnInstagram == null ||
-                btnGithub == null)
-                return;
+            Debug.Assert(
+                tvVersion != null &&
+                tvIgShVersion != null &&
+                btnTelegram != null &&
+                btnInstagram != null &&
+                btnGithub != null
+            );
 
-            tvVersion.Text = string.Format(GetString(Resource.String.msg_version), version);
+            tvVersion.Text = string.Format(
+                GetString(Resource.String.msg_app_version),
+                package.VersionName
+            );
+
+            tvIgShVersion.Text = string.Format(
+                GetString(Resource.String.msg_instasharp_version),
+                igVersion.Name,
+                igVersion.Version
+            );
+
             btnTelegram.Click += Telegram_Click;
             btnInstagram.Click += Instagram_Click;
             btnGithub.Click += Github_Click;
@@ -46,7 +70,7 @@ namespace Madamin.Unfollow.Fragments
             try
             {
                 var intent = Intent.ParseUri(
-                    "https://t.me/unfollowapp", 
+                    "https://t.me/unfollowapp",
                     IntentUriType.AndroidAppScheme);
                 Activity.StartActivity(intent);
             }
@@ -66,7 +90,7 @@ namespace Madamin.Unfollow.Fragments
             try
             {
                 var intent = Intent.ParseUri(
-                    "https://github.com/mmdmine/unfollow", 
+                    "https://github.com/mmdmine/unfollow",
                     IntentUriType.AndroidAppScheme);
                 Activity.StartActivity(intent);
             }
