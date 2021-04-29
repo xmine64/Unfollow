d1e9668d2309fd87eddd644f3647f7d4521b5455
Mohammad Amin Mollazadeh <madamin@pm.me>
2021-04-29 14:59:31 +0430

    Remove social buttons from about fragment

--- Fragments/AboutFragment.cs
+++ Fragments/AboutFragment.cs
@@ -9,12 +9,7 @@ namespace Madamin.Unfollow.Fragments
 {
     internal class AboutFragment : Fragment
     {
-        private const string TelegramChannelUrl = "https://t.me/unfollowapp";
-        private const string InstagramPageName = "minimalunfollowapp";
-        private const string GithubRepoUrl = "https://github.com/mmdmine/unfollow";
-
         private TextView versionTextView, libVersionTextView;
-        private Button telegramButton, instagramButton, githubButton;
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
@@ -29,10 +24,6 @@ namespace Madamin.Unfollow.Fragments
             versionTextView = view.FindViewById<TextView>(Resource.Id.fragment_about_app_version);
             libVersionTextView = view.FindViewById<TextView>(Resource.Id.fragment_about_instasharp_version);
 
-            telegramButton = view.FindViewById<Button>(Resource.Id.fragment_about_telegram);
-            instagramButton = view.FindViewById<Button>(Resource.Id.fragment_about_instagram);
-            githubButton = view.FindViewById<Button>(Resource.Id.fragment_about_github);
-
             // Show versions
             var versionProvider = (IVersionProvider)Activity;
             versionTextView.Text = GetString(Resource.String.msg_app_version,
@@ -43,25 +34,6 @@ namespace Madamin.Unfollow.Fragments
                 Resource.String.msg_using_x_version_y,
                 versionProvider.GetLibraryAssemblyName().Name,
                 versionProvider.GetLibraryAssemblyName().Version.ToString());
-
-            telegramButton.Click += Telegram_Click;
-            instagramButton.Click += Instagram_Click;
-            githubButton.Click += Github_Click;
-        }
-
-        private void Telegram_Click(object sender, EventArgs e)
-        {
-            ((IUrlHandler)Activity).LaunchBrowser(TelegramChannelUrl);
-        }
-
-        private void Instagram_Click(object sender, EventArgs e)
-        {
-            ((IUrlHandler)Activity).LaunchInstagram(InstagramPageName);
-        }
-
-        private void Github_Click(object sender, EventArgs e)
-        {
-            ((IUrlHandler)Activity).LaunchBrowser(GithubRepoUrl);
         }
     }
 }
